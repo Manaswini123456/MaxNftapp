a8e5973a2ff4c36847d87b217a64c30b62460861
kartikey353 <shyamsharmamohan444@gmail.com>
2022-10-10 23:53:30 +0530

    Create a responsive navbar using react

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+const Home = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="md:w-[90vw] mx-auto">
+                    <Navbar />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
