8238f12cf1923d4c303610b9511dbb3b8419222a
kartikey353 <shyamsharmamohan444@gmail.com>
2022-10-11 00:10:43 +0530

    Create responsive Home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,21 @@
-import React from 'react'
 import Navbar from '../Components/Navbar'
+import Homemaincomponent from '../Components/Homemaincomponent';
+import Trnding from '../Components/Trnding';
+import Topcollection from '../Components/Topcollection';
+import Createnft from '../Components/Createnft'; 
+import Footer from '../Components/Footer';
+
 const Home = () => {
     return (
         <>
             <div className="container">
                 <div className="md:w-[90vw] mx-auto">
                     <Navbar />
+                    <Homemaincomponent/> 
+                    <Trnding/> 
+                    <Topcollection/>  
+                    <Createnft/> 
+                    <Footer/>
                 </div>
             </div>
         </>
